77535258a7401cf2bd89b33649221dfaa22a9092
Jeremy Wall <jeremy@marzhillstudios.com>
2007-08-13 01:21:22 +0000

    Import of ZAPHAR/Bricklayer-Templater-0.9 from CPAN.
    gitpan-cpan-distribution: Bricklayer-Templater
    gitpan-cpan-version:      0.9
    gitpan-cpan-path:         ZAPHAR/Bricklayer-Templater-0.9.tar.gz
    gitpan-cpan-author:       ZAPHAR
    gitpan-cpan-maturity:     released

---
+++ lib/Bricklayer/Templater/Handler.pm
@@ -0,0 +1,65 @@
+# Bricklayer Plugin SuperClass
+package Bricklayer::Templater::Handler;
+
+#use base qw{Bricklayer::Class::Dynamic::Core};
+use Carp;
+
+# Initialization
+sub load {
+	my $PluginObj = {Token => $_[1],
+					 App => $_[2],
+					 err => undef
+					 };
+	
+    croak "ahhh didn't get passed the Token object" unless $_[1];
+    croak "ahhh didn't get passed the context object" unless $_[2];
+    $PluginObj = bless($PluginObj, ref($_[0]) || $_[0]);
+
+	$PluginObj->load_extra()
+        if $PluginObj->can('load_extra'); # optional method for handlers
+	
+	return $PluginObj;
+}
+
+sub attributes {
+	return $_[0]->{Token}{attributes};
+}
+
+sub block {
+	return $_[0]->{Token}{block};
+}
+
+sub type {
+	return $_[0]->{Token}{type};
+}
+
+sub tagname {
+	return $_[0]->{Token}{tagname};
+}
+
+sub data {
+	return $_[0]->{data} if $_[0]->{data};
+}
+
+sub tagid {
+	return $_[0]->app()->identifier();
+}
+
+sub app {
+    return $_[0]->{App};
+}
+
+sub parse_block {
+	$_[0]->app->run_sequencer($_[0]->block(), $_[2]);
+	return ;
+}
+
+sub run_handler {
+	my $result = $_[0]->run($_[1]);
+	#$_[0]->errors( "Running: ".$_[0]->tagname()." tag handler", "log");
+	#$_[0]->errors( "Sending: |$result| to publish from block: >|".$_[0]->block()."|<", "log");	
+	#$_[0]->errors( $_[0]->tagname()." had the marker: ".$_[0]->block(), "log") if ($result =~ /^ .* $/);
+	$_[0]->app()->publish($result);
+}
+
+return 1;
